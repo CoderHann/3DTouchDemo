e4290e1a4c6e99a5cb9b20e341c12e38f7e6a6ed
ITBigSea <hyt163mail@163.com>
2016-01-27 13:17:37 +0800

    copy local project

---
+++ 3DTouchDemo/3DTouchDemo/BSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSTableViewController.h
+//  3DTouchDemo
+//
+//  Created by roki on 16/1/26.
+//  Copyright © 2016年 roki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSTableViewController : UITableViewController
+
+@end
